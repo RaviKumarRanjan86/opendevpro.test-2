1f45d773c4904e2af6ba1bdac2c5824799a8dabe
Tomás Hernández <tomas.hernandez03@gmail.com>
2020-08-16 00:01:56 -0400

    Complete test 2

--- src/components/stock-data/index.js
+++ src/components/stock-data/index.js
@@ -1,23 +1,69 @@
-
 import React, { Component } from "react";
 import "./index.css";
 
 export default class StockData extends Component {
+  state = {};
+
+  handleInputChange = (event) => {
+    this.state.date = event.target.value;
+  };
+
+  handleButtonClick = async () => {
+    const request = await fetch(
+      `https://jsonmock.hackerrank.com/api/stocks?date=${this.state.date}`
+    );
+    const response = await request.json();
+    this.setState({ data: response.data });
+  };
 
   render() {
+    const { data } = this.state;
+
+    const isRequested = data;
+    const hasData = data && !!data.length;
+
     return (
       <div className="layout-column align-items-center mt-50">
         <section className="layout-row align-items-center justify-content-center">
-          <input type="text" className="large" placeholder="5-January-2000" id="app-input" data-testid="app-input"/>
-          <button className="" id="submit-button" data-testid="submit-button">Search</button>
+          <input
+            type="text"
+            className="large"
+            placeholder="5-January-2000"
+            id="app-input"
+            data-testid="app-input"
+            value={this.state.date}
+            onChange={this.handleInputChange}
+          />
+          <button
+            className=""
+            id="submit-button"
+            data-testid="submit-button"
+            onClick={this.handleButtonClick}
+          >
+            Search
+          </button>
         </section>
-        <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data" >
-          <li className="py-10"></li>
-          <li className="py-10"></li>
-          <li className="py-10"></li>
-          <li className="py-10"></li>
-        </ul>
-      <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result"></div>
+        {isRequested && hasData && (
+          <ul
+            className="mt-50 slide-up-fade-in styled"
+            id="stockData"
+            data-testid="stock-data"
+          >
+            <li className="py-10">Open: {data[0].open}</li>
+            <li className="py-10">Close: {data[0].close} </li>
+            <li className="py-10">High: {data[0].high}</li>
+            <li className="py-10">Low: {data[0].low}</li>
+          </ul>
+        )}
+        {isRequested && !hasData && (
+          <div
+            className="mt-50 slide-up-fade-in"
+            id="no-result"
+            data-testid="no-result"
+          >
+            No Results Found
+          </div>
+        )}
       </div>
     );
   }
